a1ce4369ea1a01abcd9eab5bacd3d54cc17407f3
pier <pierpaolo.penna.01998@gmail.com>
2024-10-12 23:52:24 +0200

    made something wrong before this is the right commit

---
+++ deobfuscation/decoder.py
@@ -0,0 +1,216 @@
+# Generic libraries
+import sys
+# For processing and manipulating image files (extracting) 
+import imageio as iio
+# For processing and manipulating text (decoding/decrypting)
+import base64
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from mpmath import mp
+
+stegoSignature = "Sz12&%OwC;hYRF:CVs3+5"
+stegoEnd = "Eg$r4%jjK/.U8('Pq9!B4"
+
+class SignatureNotFoundError(Exception):
+    #If the file does not contain our custom signature, we throw an exception
+    pass
+
+### AUXILIARY FUNCTIONS ###
+
+# (Written by n0rdan)
+# Try to load the image specified in the input argument
+def load_image(image_path):
+
+    # Checks whether the specified path corresponds to a png or not: if not, abort.
+    if not image_path.endswith(".png"):
+        sys.exit("Specified file is not a png. Please provide a valid path to a png file.")
+
+    try:
+        return iio.v3.imread(image_path)
+    except Exception as error:
+        # Handles file errors, same as n0rdan's "handle_errors" function
+        if isinstance(error, FileNotFoundError): 
+            sys.exit("File cannot be found at the specified path. Please provide a valid path.")
+        elif isinstance(error, PermissionError):
+            sys.exit("Insufficient permissions to read the specified file. Check your permissions.")
+        elif isinstance(error, IsADirectoryError):
+            sys.exit("Expected a file, but found a directory. Please provide a valid path to a file.")
+        else:
+            sys.exit(f"An error occurred: {error}.")
+
+def generate_pifile(length, frac, filepath):
+
+    # Just in case
+    try:
+        length = int(length)
+    except ValueError as error:
+        sys.exit("Interpreted file length cannot be parsed to an integer. Please report this to a developer!")
+    
+    mp.dps = length + 1
+    pi_value = str(mp.pi * frac)
+
+    with open(filepath, "w") as file:
+        file.write(pi_value)
+
+###
+
+### MAIN FUNCTIONS ###
+def binary_to_string(binaryPayload):
+    allCharacters = [] 
+    
+    for i in range(0, len(binaryPayload), 8):
+        byte = binaryPayload[i:i+8] #one byte of the input
+        singleCharacter = chr(int(byte, 2)) #byte to integer to character
+        allCharacters.append(singleCharacter) #insert the character in the buffer
+        
+    return "".join(allCharacters) #convert the buffer to a string
+
+# Takes a stegomedium (PNG flag) and a corresponding steganography method (e.g.: LSB) and
+# returns the payload within.
+def extract_payload(stegoImage, method):
+    stegoImageHeight = stegoImage.shape[0]
+    stegoImageWidth = stegoImage.shape[1]
+    stegoImageChannel = stegoImage.shape[2]
+    
+    readingIndex = 0
+    signatureBinary = ''.join(format(byte, '08b') for byte in stegoSignature.encode('utf-8'))
+    signatureLength = len(signatureBinary)
+    
+    embeddedPayload = ""
+    
+    stegoEndBinary = ''.join(format(byte, '08b') for byte in stegoEnd.encode('utf-8'))
+    stegoEndLength = len(stegoEndBinary)
+    stegoEndBuffer = ""
+    
+    for i in range(stegoImageHeight):
+        for j in range(stegoImageWidth):
+            for k in range(stegoImageChannel):
+                if readingIndex < signatureLength:
+                    if int(stegoImage[i][j][k] & 1) == int(signatureBinary[readingIndex]):
+                        readingIndex += 1
+                    else:
+                        #one of the first len(signature) bits does not match the corresponding bit on the signature
+                        raise SignatureNotFoundError
+                else:
+                    lsb = stegoImage[i][j][k] & 1
+                    embeddedPayload += str(lsb)
+                    stegoEndBuffer += str(lsb)
+                    
+                    if len(stegoEndBuffer) > stegoEndLength:
+                        #the length of the buffer must match the length of the end signature at all times
+                        stegoEndBuffer = stegoEndBuffer[-stegoEndLength:]
+                        
+                    if stegoEndBuffer == stegoEndBinary:
+                        print("Payload successfully extracted!\nDecoding in progress...\n")
+                        embeddedPayload = embeddedPayload[:-stegoEndLength]
+                        return binary_to_string(embeddedPayload)
+
+# Takes a payload and decodes it, reversing the steps taken in Payload-Creation (by Jantri) and
+# returning the decoded original message.
+def decode_payload(payload):
+
+    ### Step 1: extract the actual payload from the input (method will depend on whether time-based encoding is used)
+    aux_filepath = "auxconstant.txt"
+    
+    if payload[:4] == "TIME":
+        _, timestr, payload = payload.split(" ")
+        hour, minute, second = timestr.split(":")
+
+        num_of_seconds = hour * 3600 + minute * 60 + second
+        timefrac = num_of_seconds / (24 * 3600)
+
+        length = len(payload) # Does not include "TIME hh:mm:ss "
+        generate_pifile(length, frac = timefrac, filepath = aux_filepath)
+
+        with open(aux_filepath, "r") as file:
+            decimals = file.read(length + 2)[2:]
+        decimals = list(decimals)
+
+    else:
+        length = len(payload)
+        generate_pifile(length, frac = 1, filepath = aux_filepath)
+
+        with open(aux_filepath, "r") as file:
+            decimals = file.read(length + 2)[2:]
+        decimals = list(decimals)
+
+    # Obfuscation logic example (to hide the first character of the encoded payload): 
+    # - generate a 10 character long string full of random characters
+    # - get the first (index=0) decimal digit of the constant we picked (pi * frac, where frac = 1 if no time encoding is used)
+    # - use that digit as an index to pick which of the 10 random characters we're going to replace with the first character of the payload
+    # - pseudocode: random_string[k] = encoded_payload[0], where k = decimals[0]
+
+    # Deobfuscation logic example (bearing in mind we know the constant used to generate decimals[]):
+    # - for every 10-character 'chunk', extract the character whose index matches the corresponding digit of the constant
+    result = ""
+    for i in range(int(length // 10)):
+        offset = decimals[i]
+        print("result "+result)
+        print("offset "+offset)
+        print("payload "+payload[(10*i)+int(offset)])
+        result += str(payload[10*i+int(offset)])
+    
+    # Since I want to use "payload" as my variable from here onwards, we're going to replace the original value with the extraction result
+    payload = result
+
+    ### Step 2: undo the base32-encoding step
+    payload = payload.encode('utf-8')
+    payload = base64.b32decode(payload)
+
+    ### Step 3: decypher the payload
+    with open("private_key.pem", "rb") as f:
+        private_key = serialization.load_pem_private_key(f.read(), password = None)
+
+    chunks = [payload[i:i+50] for i in range(0, len(payload), 50)]
+
+    plaintext = ""
+    for chunk in chunks:
+        plaintext += private_key.decrypt(
+            chunk.decode(), 
+            padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None)
+        )
+    
+    return plaintext
+
+# ASCII art which gets printed every time when the program is executed
+def print_ascii_art():
+    art = r"""
+  _____  _   _  _____    _____ _                                                     _           
+ |  __ \| \ | |/ ____|  / ____| |                                                   | |          
+ | |__) |  \| | |  __  | (___ | |_ ___  __ _  __ _ _ __   ___   __ _ _ __ __ _ _ __ | |__  _   _ 
+ |  ___/| . ` | | |_ |  \___ \| __/ _ \/ _` |/ _` | '_ \ / _ \ / _` | '__/ _` | '_ \| '_ \| | | |
+ | |    | |\  | |__| |  ____) | ||  __/ (_| | (_| | | | | (_) | (_| | | | (_| | |_) | | | | |_| |
+ |_|    |_| \_|\_____| |_____/ \__\___|\__, |\__,_|_| |_|\___/ \__, |_|  \__,_| .__/|_| |_|\__, |
+                                        __/ |                   __/ |         | |           __/ |
+                                       |___/                   |___/          |_|          |___/                               
+________________________________________________________________________________________________,,,
+______                   _           
+|  _  \                 | |          
+| | | |___  ___ ___   __| | ___ _ __ 
+| | | / _ \/ __/ _ \ / _` |/ _ \ '__|
+| |/ /  __/ (_| (_) | (_| |  __/ |   
+|___/ \___|\___\___/ \__,_|\___|_|   
+    
+    """
+    print(art)
+
+def main():
+
+    print_ascii_art()
+
+    if len(sys.argv) < 2: # There are arguments missing for the program
+        print("Usage: python png_steganography.py <file_path_payload> <file_path_picture>")
+        print("The payload has to be a .txt file and the picture has to be a .png file")
+    else:
+        image = load_image(sys.argv[1])
+        try:
+            payload = extract_payload(image, method= "LSB")
+            result = decode_payload(payload)
+        except SignatureNotFoundError as error:
+            sys.exit("Loaded file does not contain any hidden message")
+        print("Success! The hidden message is:\n")
+        print(result)
+
+if __name__ == "__main__":
+    main()
---
+++ payload_creation/payload.py
@@ -0,0 +1,177 @@
+import sys
+#Used for ciphering the message (Step 1)
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+#Used for encoding in base 32 (Step 2)
+import base64
+from datetime import datetime
+from pi_decimals import n_of_pi_decimals 
+from pi_decimals import frac_of_pi_decimals
+
+#For step3 random chars
+import random
+import string 
+
+Use_time = False
+Cipherkey = b'ArbitraryKey'#Not used
+Outputfile = "payload.txt"
+pifile = "pi_decimals.txt"
+
+def split_string(str):
+    # We split the string into chunks of 50characters 
+    return [str[i:i+50] for i in range(0, len(str), 50)]
+
+#Step 1 cipher text
+def cipher(contents):
+    ciphertext = bytearray()
+    #We open the file where the public key is stored
+    with open("public_key.pem", "rb") as f:
+        public_key = serialization.load_pem_public_key(f.read())#Store the key
+
+    
+    chunks = split_string(contents)
+
+    for c in chunks:
+       ciphertext += public_key.encrypt(c.encode(),#encode into bytes
+                                    padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None)
+                                    )
+
+
+
+    # Encrypt the message using RSA
+    #ciphertext = public_key.encrypt(contents.encode(),#encode into bytes
+    #                                padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None)
+    #                                )
+        #We are using Optical Assymetric Encryption Padding (Padding scheme to assure randomness -this padding is NOT deterministic-)
+        # mgf=padding.MGF1(algorithm=hashes.SHA256())
+        # MGF1 (Mask Generation Function 1): 
+        # Cryptographic function used in conjunction with OAEP. 
+        # It will generates a mask (sequence of bytes used to randomize the encryption)
+        #
+        #hashes.SHA256(): 
+        # We are using SHA256 in the MGF
+        #
+        #algorithm=hashes.SHA256()
+        #We are ensuring SHA256 is the MAIN hashing function used in the OAEP
+
+    return ciphertext
+
+#Step 3 cover the message using pi decimals
+def picover(contents):
+    cover = ""
+    length = len(contents)
+    if Use_time:
+            frac = fraction_of_day(Outputfile)
+            frac_of_pi_decimals(frac,length)
+
+    if length > (10000 - 2):
+        if Use_time:
+            with open("timepi_decimals.txt", "r") as f:
+                pipositions = f.read(length+2)[2:]#read the length needed skipping the "3." in order to only have the decimals of pi
+        else:
+            n_of_pi_decimals(length)
+            with open("npi_decimals.txt", "r") as f:
+                pipositions = f.read(length+2)[2:]#read the length needed skipping the "3." in order to only have the decimals of pi
+    else:
+        if Use_time:
+            with open("timepi_decimals.txt", "r") as f:
+                pipositions = f.read(length+2)[2:]#read the length needed skipping the "3." in order to only have the decimals of pi
+        else:
+            with open("pi_decimals.txt", "r") as f:
+                pipositions = f.read(length+2)[2:]
+
+    pipositions = list(pipositions)
+    for i in range(length):
+        random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        if Use_time:
+            temp = int(int(pipositions[i])*frac)
+            substitute_char(random_string,temp,contents[i])
+        else:
+            substitute_char(random_string,int(pipositions[i]),contents[i])
+        
+        cover= cover + str(random_string)
+    
+    return cover
+
+#Substitute chars
+def substitute_char(s, position, new_char):
+    new_char = str(new_char) #As contents may have numbers
+    if position < 0 or position >= len(s):#Just in case something weird happens
+        return "Position out of range"
+    new_string = s[:position] + new_char + s[position+1:]
+    return new_string
+
+def fraction_of_day(filename):
+    now = datetime.now()
+    #We get the current hour, minute, and second
+    hours = now.hour
+    minutes = now.minute
+    seconds = now.second
+
+    #We write it to the payload file in order to place it in the metadata
+    with open(filename, 'w+') as f:
+        f.write(now.strftime("TIME %H:%M:%S \n"))
+
+    #we get the seconds since 0:0:00
+    current_seconds = hours * 3600 + minutes * 60 + seconds
+
+    #Out of all the seconds in a day
+    total_seconds_in_a_day = 24 * 3600
+
+    #We get the fraction of day that has passed since midnight to the current time
+    frac_of_day = current_seconds / total_seconds_in_a_day
+    return frac_of_day
+
+
+def main():
+    # Check if the filename is passed as an argument
+    if len(sys.argv) < 2:
+        print("Usage: python payload.py <filename> [y]")
+        print("filename from where to read the plaintext")
+        print("y: in order to have more security, use this option in order for the pi decimals to be multiplied by a fraction of the timedate")
+        sys.exit(1)
+
+    # Get the filename from the command-line argument
+    filename = sys.argv[1]
+
+    #specify number of decimals(-1)
+    if len(sys.argv) >= 3:
+        if sys.argv[2] == 'y':
+            global Use_time 
+            Use_time = True
+        
+    try:
+        #get the payload
+        with open(filename, 'r') as input_file:
+            contents = input_file.read()
+
+    
+        #Step 1 Cypher: cypher the contents
+        s1contents = cipher(contents)
+
+   
+        #Step 2 Base32(Linguistics): encode the contents to base 32
+        s2contents = base64.b32encode(s1contents)
+
+        s2contents = s2contents.decode('utf-8')
+        #Step 3 Pi(Linguistics):use pi decimals to "cover" the base 32
+        #& Step 4 TimeFraction: use a fraction of pi depending on the date time of the execution to hide it even better 
+        #If this option is ativated (using 'y') it will leave one line with the current time of the execution
+        s3contents = picover(s2contents)
+
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+        sys.exit(1)
+    
+    #Finalstep
+    try:
+        mode = 'a+' if Use_time else 'w+'
+        with open(Outputfile,mode)as output_file:
+            output_file.write(s3contents)
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
